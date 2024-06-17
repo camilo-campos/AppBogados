ca46b76a016c96ca7c576ac9c3bda40a487228d3
camilo campos <camilo.camposog@gmail.com>
2024-06-17 14:17:49 -0400

    rut en el formulario

--- appAbogados/public/js/script.js
+++ appAbogados/public/js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("JavaScript cargado y listo.");
-
-  fetch("/csrf-token")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("csrf-token").value = data.csrfToken;
-    })
-    .catch((error) => {
-      console.error("Error fetching CSRF token:", error);
-    });
-});
+//document.addEventListener("DOMContentLoaded", () => {
+//  console.log("JavaScript cargado y listo.");
+//
+//  fetch("/csrf-token")
+//    .then((response) => response.json())
+//    .then((data) => {
+//      document.getElementById("csrf-token").value = data.csrfToken;
+//    })
+//    .catch((error) => {
+//      console.error("Error fetching CSRF token:", error);
+//    });
+//});
