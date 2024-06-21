f5e95e93f329e651677a89e1c1f3298f95496471
camilo campos <camilo.camposog@gmail.com>
2024-06-21 17:31:21 -0400

    cambios dentro del inicio las imagenes de fondo

--- appAbogados/public/js/script.js
+++ appAbogados/public/js/script.js
@@ -10,3 +10,42 @@
 //      console.error("Error fetching CSRF token:", error);
 //    });
 //});
+
+document
+  .getElementById("cliente-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const Apellidos = document.getElementById("Apellidos").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const Region = document.getElementById("Region").value.trim();
+    const Comuna = document.getElementById("Comuna").value.trim();
+    const Problema = document.getElementById("Problema").value.trim();
+
+    if (!name || !Apellidos || !email || !Region || !Comuna || !Problema) {
+      alert("Todos los campos son obligatorios.");
+      return;
+    }
+
+    // Enviar formulario
+    const formData = new FormData(event.target);
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formJson = JSON.stringify(plainFormData);
+
+    fetch("/submit-form", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: formJson,
+    })
+      .then((response) => {
+        if (response.ok) {
+          window.location.href = "/success";
+        } else {
+          console.error("Error al enviar el formulario");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
